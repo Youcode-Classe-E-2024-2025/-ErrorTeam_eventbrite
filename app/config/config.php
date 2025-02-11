991ad2c6b12b55a8e270642766dfd66103c0589f
hassanbelaqdour <hassanbelaqdour@gmail.com>
2025-02-11 15:07:29 +0100

    creation de profile.twig et et profilecontroller

--- app/config/config.php
+++ app/config/config.php
@@ -3,9 +3,9 @@ return [
     'database' => [
         'driver' => 'pgsql', 
         'host' => 'localhost', 
-        'port' => '5432', 
+        'port' => '5433', 
         'dbname' => 'eventbrite', 
         'user' => 'postgres', 
-        'password' => 'belal', 
+        'password' => 'hassan', 
     ]
 ];
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'belal/event-app',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '7371aeb8528cdbe926ab97c7fc7449690306dff7',
+        'reference' => 'a8b970d1efd5bf4b275daf3c7c8d717a558708ad',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'belal/event-app' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '7371aeb8528cdbe926ab97c7fc7449690306dff7',
+            'reference' => 'a8b970d1efd5bf4b275daf3c7c8d717a558708ad',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
