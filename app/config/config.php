cf9391a64263117db3cbf705af9427d87561d6c8
belal-allala <belalallala810@gmail.com>
2025-02-12 15:39:17 +0100

    conflit

--- app/config/config.php
+++ app/config/config.php
@@ -3,9 +3,13 @@ return [
     'database' => [
         'driver' => 'pgsql', 
         'host' => 'localhost', 
-        'port' => '5433', 
+        'port' => '5432', 
         'dbname' => 'eventbrite', 
         'user' => 'postgres', 
+<<<<<<< Updated upstream
         'password' => 'hassan', 
+=======
+        'password' => 'belal', 
+>>>>>>> Stashed changes
     ]
 ];
