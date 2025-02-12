2b1d43aaf197a5185f909dcb5cd2c45bf9d378a4
belal-allala <belalallala810@gmail.com>
2025-02-12 15:39:17 +0100

    conflit

--- app/config/config.php
+++ app/config/config.php
@@ -3,9 +3,9 @@ return [
     'database' => [
         'driver' => 'pgsql', 
         'host' => 'localhost', 
-        'port' => '8885', 
+        'port' => '5432', 
         'dbname' => 'eventbrite', 
         'user' => 'postgres', 
-        'password' => 'hassan', 
+        'password' => 'belal', 
     ]
 ];
