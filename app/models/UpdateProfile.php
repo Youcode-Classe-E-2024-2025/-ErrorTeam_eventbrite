912561133b3f2d0e498ec6c43ab299f40789d6d2
hassanbelaqdour <hassanbelaqdour@gmail.com>
2025-02-15 21:55:11 +0100

    j'ai travailler sur auth with google mais pas de resultat et j'ai fais le reset a cette commit

--- app/models/UpdateProfile.php
+++ app/models/UpdateProfile.php
@@ -17,35 +17,35 @@ class UpdateProfile
 
     public function updateProfile($user, $data, $avatar = null)
     {
-        // Validation et nettoyage des données (important !)
+        
         $username = trim($data['username'] ?? $user->getUsername());
         $firstName = trim($data['first_name'] ?? $user->getFirstName());
         $lastName = trim($data['last_name'] ?? $user->getLastName());
         $phoneNumber = trim($data['phone_number'] ?? $user->getPhoneNumber());
         $bio = trim($data['bio'] ?? $user->getBio());
 
-        // Validation de la longueur des champs
+        
         if (strlen($username) > 255 || strlen($firstName) > 255 || strlen($lastName) > 255 || strlen($phoneNumber) > 20) {
             Session::set('error', 'Certains champs dépassent la longueur autorisée.');
-            return false; // Indiquer l'échec
+            return false;
         }
 
-        // Mise à jour des propriétés de l'objet User
+        
         $user->setUsername($username);
         $user->setFirstName($firstName);
         $user->setLastName($lastName);
         $user->setPhoneNumber($phoneNumber);
         $user->setBio($bio);
 
-        // Gestion de l'avatar (si fourni)
+        
         if ($avatar) {
             $user->setAvatar($avatar);
         }
 
-        // Obtenez la connexion PDO
+        
         $pdo = $this->db->getConnection();
 
-        // Préparation de la requête SQL
+        
         $stmt = $pdo->prepare("UPDATE users SET username = :username, first_name = :first_name, last_name = :last_name, phone_number = :phone_number, bio = :bio, avatar = :avatar WHERE id = :id");
         $stmt->bindValue(':username', $user->getUsername());
         $stmt->bindValue(':first_name', $user->getFirstName());
@@ -55,17 +55,17 @@ class UpdateProfile
         $stmt->bindValue(':avatar', $user->getAvatar());
         $stmt->bindValue(':id', $user->getId());
 
-        // Exécution de la requête
+        
         if ($stmt->execute()) {
-            // Mettre à jour les variables de session
+           
             $_SESSION['first_name'] = $user->getFirstName();
             $_SESSION['last_name'] = $user->getLastName();
             $_SESSION['phone_number'] = $user->getPhoneNumber();
-            return true; // Indiquer le succès
+            return true;
            
         } else {
             Session::set('error', 'Erreur lors de la mise à jour du profil.');
-            return false; // Indiquer l'échec
+            return false;
         }
     }
 }
